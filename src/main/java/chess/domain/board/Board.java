666ae5d787d7689abb11d82d1a60ba374f5044a2
seokmyungham <hellotjraud@gmail.com>
2024-04-04 01:33:05 +0900

    feat(Board): 기물 위치를 포함하는 일급 컬렉션 Board 추가

---
+++ src/main/java/chess/domain/board/Board.java
@@ -0,0 +1,51 @@
+package chess.domain.board;
+
+import chess.domain.board.position.Position;
+import chess.domain.piece.Piece;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Board {
+
+    private final Map<Position, Piece> board;
+
+    public Board(Map<Position, Piece> board) {
+        this.board = board;
+    }
+
+    public List<Piece> findKing() {
+        return board.values().stream()
+                .filter(Piece::isKing)
+                .toList();
+    }
+
+    public void movePiece(Position from, Position to) {
+        Piece fromPiece = board.get(from);
+        board.put(to, fromPiece);
+        board.remove(from);
+    }
+
+    public Piece findPieceByPosition(Position position) {
+        if (existPiece(position)) {
+            return board.get(position);
+        }
+        throw new IllegalArgumentException("해당 위치에 기물이 없습니다.");
+    }
+
+    public boolean isEmptySpace(Position position) {
+        return !existPiece(position);
+    }
+
+    public boolean existPiece(Position position) {
+        return board.containsKey(position);
+    }
+
+    public void put(Position position, Piece piece) {
+        board.put(position, piece);
+    }
+
+    public Map<Position, Piece> getBoard() {
+        return new HashMap<>(board);
+    }
+}
--- src/main/java/chess/domain/board/BoardFactory.java
+++ src/main/java/chess/domain/board/BoardFactory.java
@@ -6,51 +6,47 @@ import chess.domain.board.position.Row;
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
 import chess.domain.piece.PieceType;
-import java.util.HashMap;
-import java.util.Map;
 
 public class BoardFactory {
 
-    public Map<Position, Piece> initialize() {
-        Map<Position, Piece> map = new HashMap<>();
-        initializeBlackTeam(map);
-        initializeWhiteTeam(map);
-        return map;
+    public static void initialize(Board board) {
+        initializeBlackTeam(board);
+        initializeWhiteTeam(board);
     }
 
-    private void initializeBlackTeam(Map<Position, Piece> map) {
-        initializePawn(map, Row.SEVEN, Color.BLACK);
-        initializeHighValuePiece(map, Row.EIGHT, Color.BLACK);
+    private static void initializeBlackTeam(Board board) {
+        initializePawn(board, Row.SEVEN, Color.BLACK);
+        initializeSpecialPiece(board, Row.EIGHT, Color.BLACK);
     }
 
-    private void initializeWhiteTeam(Map<Position, Piece> map) {
-        initializePawn(map, Row.TWO, Color.WHITE);
-        initializeHighValuePiece(map, Row.ONE, Color.WHITE);
+    private static void initializeWhiteTeam(Board board) {
+        initializePawn(board, Row.TWO, Color.WHITE);
+        initializeSpecialPiece(board, Row.ONE, Color.WHITE);
     }
 
-    private void initializePawn(Map<Position, Piece> map, Row row, Color color) {
+    private static void initializePawn(Board board, Row row, Color color) {
         for (Column column : Column.values()) {
             Position position = new Position(row, column);
             if (color == Color.WHITE) {
-                map.put(position, new Piece(PieceType.WHITE_PAWN, color));
+                board.put(position, new Piece(PieceType.WHITE_PAWN, color));
             }
             if (color == Color.BLACK) {
-                map.put(position, new Piece(PieceType.BLACK_PAWN, color));
+                board.put(position, new Piece(PieceType.BLACK_PAWN, color));
             }
         }
     }
 
-    private void initializeHighValuePiece(Map<Position, Piece> map, Row row, Color color) {
-        map.put(new Position(row, Column.A), new Piece(PieceType.ROOK, color));
-        map.put(new Position(row, Column.H), new Piece(PieceType.ROOK, color));
+    private static void initializeSpecialPiece(Board board, Row row, Color color) {
+        board.put(new Position(row, Column.A), new Piece(PieceType.ROOK, color));
+        board.put(new Position(row, Column.H), new Piece(PieceType.ROOK, color));
 
-        map.put(new Position(row, Column.B), new Piece(PieceType.KNIGHT, color));
-        map.put(new Position(row, Column.G), new Piece(PieceType.KNIGHT, color));
+        board.put(new Position(row, Column.B), new Piece(PieceType.KNIGHT, color));
+        board.put(new Position(row, Column.G), new Piece(PieceType.KNIGHT, color));
 
-        map.put(new Position(row, Column.C), new Piece(PieceType.BISHOP, color));
-        map.put(new Position(row, Column.F), new Piece(PieceType.BISHOP, color));
+        board.put(new Position(row, Column.C), new Piece(PieceType.BISHOP, color));
+        board.put(new Position(row, Column.F), new Piece(PieceType.BISHOP, color));
 
-        map.put(new Position(row, Column.D), new Piece(PieceType.QUEEN, color));
-        map.put(new Position(row, Column.E), new Piece(PieceType.KING, color));
+        board.put(new Position(row, Column.D), new Piece(PieceType.QUEEN, color));
+        board.put(new Position(row, Column.E), new Piece(PieceType.KING, color));
     }
 }
