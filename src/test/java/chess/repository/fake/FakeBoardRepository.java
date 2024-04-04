d7aea9f9f73979b8743a09341c01da997dcdb56b
seokmyungham <hellotjraud@gmail.com>
2024-04-04 17:20:52 +0900

    refactor: fake repository 개선 리팩토링

--- src/test/java/chess/repository/fake/FakeBoardRepository.java
+++ src/test/java/chess/repository/fake/FakeBoardRepository.java
@@ -1,25 +1,17 @@
 package chess.repository.fake;
 
-import chess.domain.board.position.Column;
 import chess.domain.board.position.Position;
-import chess.domain.board.position.Row;
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
 import chess.domain.piece.PieceType;
 import chess.repository.BoardRepository;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
 
 public class FakeBoardRepository implements BoardRepository {
 
     @Override
     public void savePiece(Piece piece, Position position, Long roomId) {
-    }
-
-    @Override
-    public boolean existsPieceByPosition(Position position, Long roomId) {
-        return true;
     }
 
     @Override
@@ -32,31 +24,12 @@ public class FakeBoardRepository implements BoardRepository {
     }
 
     @Override
-    public List<Piece> findPiecesByColor(Color piece_color, Long roomId) {
-        return List.of(
-                new Piece(PieceType.WHITE_PAWN, Color.WHITE),
-                new Piece(PieceType.WHITE_PAWN, Color.WHITE),
-                new Piece(PieceType.WHITE_PAWN, Color.WHITE),
-                new Piece(PieceType.WHITE_PAWN, Color.WHITE),
-                new Piece(PieceType.ROOK, Color.WHITE));
-    }
-
-    @Override
-    public List<Integer> getPieceCountByPieceType(PieceType pieceType, Long roomId) {
-        return List.of(2, 2);
-    }
-
-    @Override
     public Map<Position, Piece> findAllPieceByRoomId(Long roomId) {
-        Map<Position, Piece> map = new HashMap<>();
-        map.put(new Position(Row.TWO, Column.C), new Piece(PieceType.WHITE_PAWN, Color.WHITE));
-        return map;
+        return Collections.emptyMap();
     }
 
     @Override
-    public List<Piece> findPieceByPieceType(PieceType pieceType, Long roomId) {
-        return List.of(
-                new Piece(PieceType.KING, Color.WHITE),
-                new Piece(PieceType.KING, Color.BLACK));
+    public boolean isExistsPiece(Long roomId) {
+        return true;
     }
 }
--- src/test/java/chess/repository/fake/FakeRoomRepository.java
+++ src/test/java/chess/repository/fake/FakeRoomRepository.java
@@ -4,36 +4,36 @@ import chess.domain.game.Room;
 import chess.domain.game.RoomName;
 import chess.domain.piece.Color;
 import chess.repository.RoomRepository;
+import java.util.Collections;
 import java.util.List;
 
 public class FakeRoomRepository implements RoomRepository {
+
     @Override
     public List<String> findAllRoomNames() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
-    public boolean existsRoomName(RoomName roomName) {
-        return false;
+    public boolean isExistsRoomName(RoomName roomName) {
+        return true;
     }
 
     @Override
     public void saveRoom(RoomName roomName) {
-
     }
 
     @Override
     public void updateRoomTurn(Color color, Long roomId) {
-
     }
 
     @Override
     public Room findRoomByName(RoomName roomName) {
-        return null;
+        return new Room(0L, roomName.getValue());
     }
 
     @Override
     public Color findTurnById(Long roomId) {
-        return Color.BLACK;
+        return Color.WHITE;
     }
 }
